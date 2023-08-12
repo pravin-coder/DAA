bbcb803264610378154b0482738eed8f68c3c66f
pravin-coder <113326783+pravin-coder@users.noreply.github.com>
2023-08-12 08:42:47 +0530

    EXP2.C

---
+++ EXP 2.C
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function to calculate the number of digits in a number
+int countDigits(int num) {
+    int count = 0;
+    while (num != 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+// Function to check if a number is an Armstrong number
+int isArmstrong(int num) {
+    int originalNum = num;
+    int n = countDigits(num);
+    int sum = 0;
+
+    while (num != 0) {
+        int digit = num % 10;
+        sum += pow(digit, n);
+        num /= 10;
+    }
+
+    return sum == originalNum;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isArmstrong(num)) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+
+    return 0;
+}
