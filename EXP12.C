d203f57e74575a111e06ac14f15a4583f40f0abb
pravin-coder <113326783+pravin-coder@users.noreply.github.com>
2023-08-12 08:51:22 +0530

    EXP12.C

---
+++ EXP12.C
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// Function to perform binary search
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+    
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == target) {
+            return mid;  // Element found at index mid
+        }
+        
+        if (arr[mid] < target) {
+            left = mid + 1;  // Search in the right half
+        } else {
+            right = mid - 1;  // Search in the left half
+        }
+    }
+    
+    return -1;  // Element not found
+}
+
+int main() {
+    int size, target;
+
+    printf("Enter the size of the sorted array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter the elements of the sorted array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the target element to search: ");
+    scanf("%d", &target);
+
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1) {
+        printf("Element %d found at index %d.\n", target, result);
+    } else {
+        printf("Element %d not found in the array.\n", target);
+    }
+
+    return 0;
+}
