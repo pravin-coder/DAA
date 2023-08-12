1db679c9c3086869fc5ffaedb0a9e581d3148ea6
pravin-coder <113326783+pravin-coder@users.noreply.github.com>
2023-08-12 09:22:17 +0530

    Create EXP37.C

---
+++ EXP37.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void printFactors(int n) {
+    printf("Factors of %d: ", n);
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid input. Please enter a positive integer.\n");
+        return 1;
+    }
+
+    printFactors(n);
+
+    return 0;
+}
