bad2f22b9fe826b1e671ea6e52eca5981aa0b7e0
pravin-coder <113326783+pravin-coder@users.noreply.github.com>
2023-08-12 08:48:12 +0530

    EXP8.C

---
+++ EXP8.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// Function to perform bubble sort
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    bubbleSort(arr, size);
+    
+    printf("Sorted array in ascending order:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
