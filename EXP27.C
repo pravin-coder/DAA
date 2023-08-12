a13ddf4642b06510cdde57c4dab16e9a699e95fa
pravin-coder <113326783+pravin-coder@users.noreply.github.com>
2023-08-12 09:15:03 +0530

    Create EXP27.C

---
+++ EXP27.C
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define V 4  // Number of vertices
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+
+    // Initialize dist matrix with initial values
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    // Find shortest paths
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX
+                    && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print the shortest path matrix
+    printf("Shortest path distances between all pairs of vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INT_MAX) {
+                printf("INF\t");
+            } else {
+                printf("%d\t", dist[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 5, INT_MAX, 10},
+        {INT_MAX, 0, 3, INT_MAX},
+        {INT_MAX, INT_MAX, 0, 1},
+        {INT_MAX, INT_MAX, INT_MAX, 0}
+    };
+
+    floydWarshall(graph);
+
+    return 0;
+}
